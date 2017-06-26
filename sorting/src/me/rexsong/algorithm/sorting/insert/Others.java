8f699aa3c69d0f0dffd5e22abb3a7b3d0f3faeb8
MagicRexSong <songsx-a@glodon.com>
2017-06-26 22:24:00 +0800

    init

---
+++ sorting/src/me/rexsong/algorithm/sorting/insert/Others.java
@@ -0,0 +1,10 @@
+package me.rexsong.algorithm.sorting.insert;
+
+/**
+ * Created by RexSong on 2017/6/26.
+ */
+public class Others {
+    public static void main(String[] args) {
+        
+    }
+}
